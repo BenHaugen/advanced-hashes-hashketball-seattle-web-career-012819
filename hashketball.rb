5a14f1b587abda97358cd5cbc4f10515edd15388
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-26 04:02:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -213,13 +213,13 @@ def big_shoe_rebounds
   rebounds = nil
   shoe_size = nil
   game_hash.each do |team, data|
-    data[team][:players].each do |category, number|
+    game_hash[team][:players].each do |player, stats|
     binding.pry
     end
   end
 end
 
-  
+  g
       
   
 
